69b6d287e6f9e0c26a0ca5bf505cac2bf838932a
Marcelo Adrian Oliveto <marcelo.oliveto@gmail.com>
2023-12-10 03:42:46 -0300

    navbars

--- src/components/NavBar2.jsx
+++ src/components/NavBar2.jsx
@@ -1,95 +1,22 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import AccountCircle from '@mui/icons-material/AccountCircle';
-import Switch from '@mui/material/Switch';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import FormGroup from '@mui/material/FormGroup';
-import MenuItem from '@mui/material/MenuItem';
-import Menu from '@mui/material/Menu';
+import React from 'react'
+import { Link } from 'react-router-dom'
+import CartWidget from '../../../tercera entrega/src/components/CartWidget'
 
-export default function NavBar2() {
-  const [auth, setAuth] = React.useState(true);
-  const [anchorEl, setAnchorEl] = React.useState(null);
-
-  const handleChange = (event) => {
-    setAuth(event.target.checked);
-  };
-
-  const handleMenu = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-
-  return (
-    <Box sx={{ flexGrow: 1 }}>
-      <FormGroup>
-        <FormControlLabel
-          control={
-            <Switch
-              checked={auth}
-              onChange={handleChange}
-              aria-label="login switch"
-            />
-          }
-          label={auth ? 'Logout' : 'Login'}
-        />
-      </FormGroup>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Photos
-          </Typography>
-          {auth && (
-            <div>
-              <IconButton
-                size="large"
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                onClick={handleMenu}
-                color="inherit"
-              >
-                <AccountCircle />
-              </IconButton>
-              <Menu
-                id="menu-appbar"
-                anchorEl={anchorEl}
-                anchorOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
-                open={Boolean(anchorEl)}
-                onClose={handleClose}
-              >
-                <MenuItem onClick={handleClose}>Profile</MenuItem>
-                <MenuItem onClick={handleClose}>My account</MenuItem>
-              </Menu>
-            </div>
-          )}
-        </Toolbar>
-      </AppBar>
-    </Box>
-  );
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <Link to="/" className="logo"><h1>MAO Shop</h1></Link>
+            <ul className="menu">
+                <li><Link className="menu-link" to="/">Inicio</Link></li>
+                <li><Link className="menu-link" to="/products">Productos</Link></li>
+                <li><Link className="menu-link" to="/products/Almacen">Almacen</Link></li>
+                <li><Link className="menu-link" to="/products/Bebidas">Bebidas</Link></li>
+                <li><Link className="menu-link" to="/aboutus">Nosotros</Link></li>
+                <li><Link className="menu-link" to="/contact">Contacto</Link></li>
+                <li><CartWidget /></li>
+            </ul>
+        </nav>
+    )
 }
+
+export default Navbar
